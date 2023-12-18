3538864b0d7518cb832406f8d0d68a017fc7ac3d
ibrarian9 <ibrarian9@gmail.com>
2023-12-18 09:27:45 +0700

    Initial commit

---
+++ app/src/main/java/com/app/raptor/HasilLaporanActivity.java
@@ -0,0 +1,14 @@
+package com.app.raptor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HasilLaporanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_laporan);
+    }
+}
