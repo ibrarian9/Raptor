85d8429037beaa9612d503955c162fd37e2e67cc
aslab <ibrarian9@gmail.com>
2023-12-19 17:01:35 +0700

    Menambahkan DashboardKpActivity.java

--- app/src/main/java/com/app/raptor/ListLaporanActivity.java
+++ app/src/main/java/com/app/raptor/ListLaporanActivity.java
@@ -2,16 +2,19 @@ package com.app.raptor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CalendarView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ListLaporanActivity extends AppCompatActivity {
 
     FloatingActionButton plusBtn;
     CalendarView calender;
+    BottomNavigationView botNavbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,7 @@ public class ListLaporanActivity extends AppCompatActivity {
 
         plusBtn = findViewById(R.id.fabAdd);
         calender = findViewById(R.id.cv);
+        botNavbar = findViewById(R.id.bottomNavigationView);
         calender.setVisibility(View.GONE);
         System.out.println(calender.getVisibility());
 
@@ -30,5 +34,21 @@ public class ListLaporanActivity extends AppCompatActivity {
         }
 
         System.out.println(calender.getVisibility());
+
+        botNavbar.setOnItemSelectedListener( i -> {
+            int id = i.getItemId();
+            if (id == R.id.beranda){
+                return true;
+            } else if (id == R.id.cari) {
+                startActivity(new Intent(this, ListLaporanActivity.class));
+                return true;
+            } else if (id == R.id.agenda) {
+                return true;
+            } else if (id == R.id.profil) {
+                return true;
+            }
+            return false;
+        });
+
     }
 }
